bf3c3d5ec6b80fa55818474aeb4dee844ef3b297
tjchambers32 <tjchambers32@gmail.com>
2015-01-29 12:08:58 -0700

    Create Pixel.java

---
+++ Pixel.java
@@ -0,0 +1,33 @@
+
+public class Pixel {
+	
+	private int red = 0;
+	private int green = 0;
+	private int blue = 0;
+	
+	public int getRed() {
+		return red;
+	}
+	public void setRed(int red) {
+		this.red = red;
+	}
+	public int getGreen() {
+		return green;
+	}
+	public void setGreen(int green) {
+		this.green = green;
+	}
+	public int getBlue() {
+		return blue;
+	}
+	public void setBlue(int blue) {
+		this.blue = blue;
+	}
+	
+	public String toString()
+	{
+		String str = null;
+		str = red + " " + green + " " + blue + "\n";
+		return str;
+	}
+}
